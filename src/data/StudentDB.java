041b2732223ef0a9c0883e553cc6be56951fce37
Sarah Snyder <slsnyder@uw.edu>
2016-11-30 10:31:05 -0800

    Added basic functionality to StudentDB and Student

--- src/data/StudentDB.java
+++ src/data/StudentDB.java
@@ -1,5 +1,69 @@
 package data;
 
-public class StudentDB {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import student.Student;
+
+public class StudentDB {
+	
+	private Connection mConnection;
+	private List<Student> mStudentList;
+	
+	/**
+	 * Retrieves all items from the Item table.
+	 * 
+	 * @return list of items
+	 * @throws SQLException
+	 */
+	public List<Student> getStudents() throws SQLException {
+		if (mConnection == null) {
+			mConnection = DataConnection.getConnection();
+		}
+		Statement stmt = null;
+		String query = "select * " + "from Student ";
+
+		mStudentList = new ArrayList<Student>();
+		try {
+			stmt = mConnection.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				int id = rs.getInt("studentId");
+				String lastName = rs.getString("lastName");
+				String firstName = rs.getString("firstName");
+				String academicProgram = rs.getString("academicProgram");
+				String degreeLevel = rs.getString("degreeLevel");
+				String gradTerm = rs.getString("gradTerm");
+				int gradYear = rs.getInt("gradYear");
+				double gpa = rs.getDouble("gpa");
+				String uwEmail = rs.getString("uwEmail");
+				String externalEmail = rs.getString("externalEmail");
+				
+				Student student = null;
+				// TODO create student
+				// TODO include tranfer/ internship/ employment ?
+				/*if (desc == null) {
+					item = new Item(name, new ItemCategory(category));
+					item.setId(new Integer(id).toString());
+				} else {
+					item = new Item(name, desc, price, condition,
+							new ItemCategory(category));
+					item.setId(new Integer(id).toString());
+				}*/
+				mStudentList.add(student);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println(e);
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
+		}
+		return mStudentList;
+	}
 }
--- src/student/Student.java
+++ src/student/Student.java
@@ -1,5 +1,107 @@
 package student;
 
-public class Student {
+// TODO input validation
 
+public class Student {
+	
+	private String mId;
+	private String mLastName;
+	private String mFirstName;
+	private String mAcademicProgram;
+	private String mDegreeLevel;
+	private String mGradTerm;
+	private String mGradYear;
+	private double mGpa;
+	private String mUwEmail;
+	private String mExternalEmail;
+	
+	
+	/** @return student id */
+	public String getId() {
+		return mId;
+	}
+
+	/** @return last name */
+	public String getLastName() {
+		return mLastName;
+	}
+
+	public void setLastName(String lastName) {
+		mLastName = lastName;
+	}
+
+	/** @return first name */
+	public String getFirstName() {
+		return mFirstName;
+	}
+
+	public void setFirstName(String firstName) {
+		mFirstName = firstName;
+	}
+
+	/** @return academic program */
+	public String getAcademicProgram() {
+		return mAcademicProgram;
+	}
+
+	public void setAcademicProgram(String academicProgram) {
+		mAcademicProgram = academicProgram;
+	}
+
+	/** @return degree level */
+	public String getDegreeLevel() {
+		return mDegreeLevel;
+	}
+
+	public void setDegreeLevel(String degreeLevel) {
+		mDegreeLevel = degreeLevel;
+	}
+
+	/** @return graduation term */
+	public String getGradTerm() {
+		return mGradTerm;
+	}
+
+	public void setGradTerm(String gradTerm) {
+		mGradTerm = gradTerm;
+	}
+
+	/** @return graduation year */
+	public String getGradYear() {
+		return mGradYear;
+	}
+
+	public void setGradYear(String gradYear) {
+		mGradYear = gradYear;
+	}
+
+	/** @return grade point average */
+	public double getGpa() {
+		return mGpa;
+	}
+
+	public void setGpa(double gpa) {
+		mGpa = gpa;
+	}
+
+	/** @return UW email address */
+	public String getUwEmail() {
+		return mUwEmail;
+	}
+
+	public void setUwEmail(String uwEmail) {
+		mUwEmail = uwEmail;
+	}
+
+	/** @return external (non-UW) email address */
+	public String getExternalEmail() {
+		return mExternalEmail;
+	}
+
+	public void setExternalEmail(String externalEmail) {
+		mExternalEmail = externalEmail;
+	}
+	
+	
+	
 }
