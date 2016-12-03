aae46a766e314ba18083fb2e9b1ee87241e98645
Sarah Snyder <slsnyder@uw.edu>
2016-12-03 10:05:12 -0800

    Implemented StudentCollection, added needed methods to StudentDB.

--- src/data/StudentDB.java
+++ src/data/StudentDB.java
@@ -14,10 +14,10 @@ import student.CollegeTransfer;
 import student.Student;
 
 public class StudentDB {
-	
+
 	private Connection mConnection;
 	private List<Student> mStudentList;
-	
+
 	/**
 	 * Retrieves all students from the Student table.
 	 * 
@@ -46,12 +46,17 @@ public class StudentDB {
 				double gpa = rs.getDouble("gpa");
 				String uwEmail = rs.getString("uwEmail");
 				String externalEmail = rs.getString("externalEmail");
-				
+
 				String yearStr = new Integer(gradYear).toString();
-				Student student = new Student(lastName, firstName, academicProgram,
-						degreeLevel, gradTerm, yearStr, gpa, uwEmail, externalEmail);
+				Student student = new Student(lastName, firstName,
+						academicProgram, degreeLevel, gradTerm, yearStr, gpa,
+						uwEmail, externalEmail);
 				student.setId(new Integer(id).toString());
-				
+
+				setupCollegeTransfers(student);
+				setupEmployment(student);
+				setupInternships(student);
+
 				mStudentList.add(student);
 			}
 		} catch (SQLException e) {
@@ -62,120 +67,139 @@ public class StudentDB {
 				stmt.close();
 			}
 		}
-		
-		setupCollegeTransfers();
-		setupEmployment();
-		setupInternship();
-		
+
 		return mStudentList;
 	}
-	
-	private void setupCollegeTransfers() throws SQLException {
+
+	/**
+	 * Returns all students that contain the search keyword in the first name or
+	 * last name.
+	 * 
+	 * @param name
+	 * @return list of students that match.
+	 * @throws SQLException
+	 */
+	public List<Student> getStudents(String name) throws SQLException {
+		List<Student> filterList = new ArrayList<Student>();
+		if (mStudentList == null) {
+			getStudents();
+		}
+		name = name.toLowerCase();
+		for (Student student : mStudentList) {
+			if (student.getFirstName().toLowerCase().contains(name)) {
+				filterList.add(student);
+			} else if (student.getLastName().toLowerCase().contains(name)) {
+				filterList.add(student);
+			}
+		}
+		return filterList;
+	}
+
+	private void setupCollegeTransfers(Student student) throws SQLException {
 		if (mConnection == null) {
 			mConnection = DataConnection.getConnection();
 		}
-		
+
 		Statement stmt = null;
 		String query;
-		
-		for(Student student : mStudentList) {
-			try {
-				query = "select * " + "from CollegeTransfer where studentId =  " + student.getId();
-				stmt = mConnection.createStatement();
-				ResultSet rs = stmt.executeQuery(query);
-				while (rs.next()) {
-					String schoolName = rs.getString("schoolName");
-					double gpa = rs.getDouble("transferGpa");
-					String year = new Integer(rs.getInt("transferYear")).toString();
-					
-					CollegeTransfer ct = new CollegeTransfer(schoolName, gpa, year);
-					
-					student.addCollegeTransfer(ct);
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-				System.out.println(e);
-			} finally {
-				if (stmt != null) {
-					stmt.close();
-				}
+
+		try {
+			query = "select * " + "from CollegeTransfer where studentId =  "
+					+ student.getId();
+			stmt = mConnection.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				String schoolName = rs.getString("schoolName");
+				double gpa = rs.getDouble("transferGpa");
+				String year = new Integer(rs.getInt("transferYear")).toString();
+
+				CollegeTransfer ct = new CollegeTransfer(schoolName, gpa, year);
+
+				student.addCollegeTransfer(ct);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println(e);
+		} finally {
+			if (stmt != null) {
+				stmt.close();
 			}
 		}
 	}
-	
-	private void setupEmployment() throws SQLException {
+
+	private void setupEmployment(Student student) throws SQLException {
 		if (mConnection == null) {
 			mConnection = DataConnection.getConnection();
 		}
-		
+
 		Statement stmt = null;
 		String query;
-		
-		for(Student student : mStudentList) {
+
+		try {
+			query = "select * " + "from Employment where studentId =  "
+					+ student.getId() + " and internship = 0";
+			stmt = mConnection.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				String companyName = rs.getString("companyName");
+				boolean current = true;
+				if (rs.getShort("current") == 0) {
+					current = false;
+				}
+				Employment e = new Employment(new Company(companyName), current);
+
+				String desc = rs.getString("positionDescription");
+				int salary = rs.getInt("salary");
+				if (desc != null) {
+					e.setDescription(desc);
+				}
+				if (salary != 0) {
+					e.setSalary(salary);
+				}
+
+				student.addJob(e);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println(e);
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
+		}
+	}
+
+	private void setupInternships(Student student) throws SQLException {
+		if (mConnection == null) {
+			mConnection = DataConnection.getConnection();
+		}
+
+		Statement stmt = null;
+		String query;
+
 			try {
-				query = "select * " + "from Employment where studentId =  " + student.getId() + " and internship = 0";
+				query = "select * " + "from Employment where studentId =  "
+						+ student.getId() + " and internship <> 0";
 				stmt = mConnection.createStatement();
 				ResultSet rs = stmt.executeQuery(query);
 				while (rs.next()) {
 					String companyName = rs.getString("companyName");
 					boolean current = true;
-					if(rs.getShort("current") == 0) {
+					if (rs.getShort("current") == 0) {
 						current = false;
 					}
-					Employment e = new Employment(new Company(companyName), current);
-					
+					Employment e = new Employment(new Company(companyName),
+							current);
+
 					String desc = rs.getString("positionDescription");
 					int salary = rs.getInt("salary");
-					if(desc != null) {
+					if (desc != null) {
 						e.setDescription(desc);
 					}
-					if(salary != 0) {
+					if (salary != 0) {
 						e.setSalary(salary);
 					}
-					
-					student.addJob(e);
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-				System.out.println(e);
-			} finally {
-				if (stmt != null) {
-					stmt.close();
-				}
-			}
-		}
-	}
-	
-	private void setupInternship() throws SQLException {
-		if (mConnection == null) {
-			mConnection = DataConnection.getConnection();
-		}
-		
-		Statement stmt = null;
-		String query;
-		
-		for(Student student : mStudentList) {
-			try {
-				query = "select * " + "from Employment where studentId =  " + student.getId() + " and internship <> 0";
-				stmt = mConnection.createStatement();
-				ResultSet rs = stmt.executeQuery(query);
-				while (rs.next()) {
-					String companyName = rs.getString("companyName");
-					boolean current = true;
-					if(rs.getShort("current") == 0) {
-						current = false;
-					}
-					Employment e = new Employment(new Company(companyName), current);
-					
-					String desc = rs.getString("positionDescription");
-					int salary = rs.getInt("salary");
-					if(desc != null) {
-						e.setDescription(desc);
-					}
-					if(salary != 0) {
-						e.setSalary(salary);
-					}
-					
+
 					student.addInternship(e);
 				}
 			} catch (SQLException e) {
@@ -186,13 +210,14 @@ public class StudentDB {
 					stmt.close();
 				}
 			}
-		}
 	}
-	
+
 	/**
-	 * Adds a new student to the Student table. 
+	 * Adds a new student to the Student table.
+	 * 
 	 * @param student
-	 * @return Returns "Added Student successfully" or "Error adding Student: " with the sql exception.
+	 * @return Returns "Added Student successfully" or "Error adding Student: "
+	 *         with the sql exception.
 	 */
 	public String addStudent(Student student) {
 		String sql = "insert into Student(lastName, firstName, academicProgram, "
@@ -215,17 +240,51 @@ public class StudentDB {
 			preparedStatement.setString(3, student.getAcademicProgram());
 			preparedStatement.setString(4, student.getDegreeLevel());
 			preparedStatement.setString(5, student.getGradTerm());
-			preparedStatement.setInt(6, Integer.parseInt(student.getGradYear()));
+			preparedStatement
+					.setInt(6, Integer.parseInt(student.getGradYear()));
 			preparedStatement.setDouble(7, student.getGpa());
 			preparedStatement.setString(8, student.getUwEmail());
 			preparedStatement.setString(9, student.getExternalEmail());
 			preparedStatement.executeUpdate();
-			//TODO college transfer / employment / internship ?
+			// TODO college transfer / employment / internship ?
 		} catch (SQLException e) {
 			e.printStackTrace();
-			return "Error adding Student: " + e.getMessage();
+			return "Error adding student: " + e.getMessage();
 		}
-		return "Added Student successfully";
+		return "Added Student Successfully";
+	}
+
+	/**
+	 * Modifies the data on a Student // TODO transfers, employment, internships
+	 * cannot be changed here
+	 * 
+	 * @param row
+	 * @param columnName
+	 * @param data
+	 * @return Returns a message with success or failure.
+	 */
+	public String updateStudent(Student student, String columnName, Object data) {
+
+		int id = Integer.parseInt(student.getId());
+		String sql = "update Student set `" + columnName
+				+ "` = ?  where studentId = ? ";
+		// For debugging - System.out.println(sql);
+		PreparedStatement preparedStatement = null;
+		try {
+			preparedStatement = mConnection.prepareStatement(sql);
+			if (data instanceof String)
+				preparedStatement.setString(1, (String) data);
+			else if (data instanceof Double)
+				preparedStatement.setDouble(1, (Double) data);
+			preparedStatement.setInt(2, id); // for id = ?
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e);
+			e.printStackTrace();
+			return "Error updating student: " + e.getMessage();
+		}
+		return "Updated Student Successfully";
+
 	}
 
 	/**
@@ -258,5 +317,110 @@ public class StudentDB {
 			}
 		}
 		return list.toArray();
+	}
+
+	/**
+	 * Modifies the data on an Company - Only name can be modified.
+	 * 
+	 * @param company
+	 *            to modify
+	 * @param data
+	 * @return Returns a message with success or failure.
+	 */
+	public String updateCompany(String company, String data) {
+		String sql = "update Company set companyName= ?  where companyName= ?";
+		// For debugging - System.out.println(sql);
+		PreparedStatement preparedStatement = null;
+		try {
+			preparedStatement = mConnection.prepareStatement(sql);
+			preparedStatement.setString(1, data);
+			preparedStatement.setString(2, company);
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println(e);
+			e.printStackTrace();
+			return "Error updating category: " + e.getMessage();
+		}
+		return "Updated Category Successfully";
+	}
+
+	/**
+	 * Adds a new company to the Company table. 
+	 * @param company
+	 * @return Returns "Added Company Successfully" or "Error adding company: " with the sql exception.
+	 */
+	public String addCompany(Company company) {
+		String sql = "insert into Company values "
+				+ "(?); ";
+
+		if (mConnection == null) {
+			try {
+				mConnection = DataConnection.getConnection();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		PreparedStatement preparedStatement = null;
+		try {
+			preparedStatement = mConnection.prepareStatement(sql);
+			preparedStatement
+					.setString(1, company.getCompanyName());
+			preparedStatement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return "Error adding company: " + e.getMessage();
+		}
+		return "Added Company Successfully";
+	}
+
+	/**
+	 * Retrieve the student with the given id or null if not found.
+	 * 
+	 * @param id
+	 * @return student
+	 * @throws SQLException
+	 */
+	public Student getStudent(String id) throws SQLException {
+		if (mConnection == null) {
+			mConnection = DataConnection.getConnection();
+		}
+		Statement stmt = null;
+		String query = "select * " + "from Student where studentId = " + id;
+
+		try {
+			stmt = mConnection.createStatement();
+			ResultSet rs = stmt.executeQuery(query);
+			if (rs.next()) {
+				String lastName = rs.getString("lastName");
+				String firstName = rs.getString("firstName");
+				String academicProgram = rs.getString("academicProgram");
+				String degreeLevel = rs.getString("degreeLevel");
+				String gradTerm = rs.getString("gradTerm");
+				int gradYear = rs.getInt("gradYear");
+				double gpa = rs.getDouble("gpa");
+				String uwEmail = rs.getString("uwEmail");
+				String externalEmail = rs.getString("externalEmail");
+
+				String yearStr = new Integer(gradYear).toString();
+				Student student = new Student(lastName, firstName,
+						academicProgram, degreeLevel, gradTerm, yearStr, gpa,
+						uwEmail, externalEmail);
+				student.setId(new Integer(id).toString());
+
+				setupCollegeTransfers(student);
+				setupEmployment(student);
+				setupInternships(student);
+				
+				return student;
+			}
+		} catch (SQLException e) {
+			System.out.println(e);
+		} finally {
+			if (stmt != null) {
+				stmt.close();
+			}
+		}
+		return null;
 	}
 }
--- src/student/StudentCollection.java
+++ src/student/StudentCollection.java
@@ -1,5 +1,159 @@
 package student;
 
-public class StudentCollection {
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import data.StudentDB;
+import employment.Company;
+
+/**
+ * 
+ * @author Sarah Snyder
+ *
+ */
+public class StudentCollection {
+	public static StudentDB mStudentDB;
+
+	/**
+	 * Return a list of students with the matching name (first OR last).
+	 * 
+	 * @param name
+	 *            student to look for
+	 * @return a list of students that match
+	 */
+	public static List<Student> search(String name) {
+		List<Student> list = new ArrayList<Student>();
+		if (mStudentDB == null) {
+			mStudentDB = new StudentDB();
+		}
+		try {
+			return mStudentDB.getStudents(name);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	/**
+	 * Modify the particular column of the student with the given data.
+	 * 
+	 * @param student
+	 *            Student to modify
+	 * @param column
+	 *            the field in the table to modify
+	 * @param data
+	 *            the value for the field
+	 * @return true or false
+	 */
+	public static boolean update(Student student, String column, Object data) {
+		if (mStudentDB == null) {
+			mStudentDB = new StudentDB();
+		}
+		// TODO make sure they're not trying to update studentId?
+		String message = mStudentDB.updateStudent(student, column, data);
+		if (message.startsWith("Error updating student: ")) {
+			return false;
+		}
+		return true;
+	}
+
+	public static boolean updateCompany(String company, String data) {
+		if (mStudentDB == null) {
+			mStudentDB = new StudentDB();
+		}
+
+		String message = mStudentDB.updateCompany(company, data);
+		if (message.startsWith("Error updating company: ")) {
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Returns all companies available.
+	 * 
+	 * @return an array of companies.
+	 */
+	public static Object[] getCompanies() {
+		if (mStudentDB == null) {
+			mStudentDB = new StudentDB();
+		}
+
+		try {
+			return mStudentDB.getCompanies();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+
+	/**
+	 * Adds a new Student to the data.
+	 * 
+	 * @param student
+	 * @return true or false
+	 */
+	public static boolean add(Student student) {
+		if (mStudentDB == null) {
+			mStudentDB = new StudentDB();
+		}
+
+		String message = mStudentDB.addStudent(student);
+		if (message.startsWith("Error adding student:")) {
+			return false;
+		}
+		return true;
+	}
+
+	public static boolean addCompany(Company company) {
+		if (mStudentDB == null) {
+			mStudentDB = new StudentDB();
+		}
+
+		String message = mStudentDB.addCompany(company);
+		if (message.startsWith("Error adding company:")) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * 
+	 * Return a student with the given id, null otherwise.
+	 * 
+	 * @param id
+	 *            student to look for
+	 * @return student
+	 */
+	public static Student getStudent(String id) {
+		if (mStudentDB == null) {
+			mStudentDB = new StudentDB();
+		}
+		try {
+			return mStudentDB.getStudent(id);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * Return all students in the list, null otherwise.
+	 * 
+	 * @return list of students
+	 */
+	public static List<Student> getStudents() {
+		if (mStudentDB == null) {
+			mStudentDB = new StudentDB();
+		}
+		try {
+			return mStudentDB.getStudents();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
